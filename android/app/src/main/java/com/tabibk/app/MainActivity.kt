d064dafa204633910fdacd081afeb73e31c94628
cherifmimo37-stack <cherifmimo37@gmail.com>
2026-09-16 00:28:25 +0200

    Update MainActivity.kt

--- android/app/src/main/java/com/tabibk/app/MainActivity.kt
+++ android/app/src/main/java/com/tabibk/app/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.tabibk.app
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.ViewGroup
 import android.webkit.CookieManager
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.webkit.WebChromeClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -17,50 +19,104 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // لون خلفية التطبيق
+        window.statusBarColor = Color.rgb(43, 11, 61)
+        window.navigationBarColor = Color.rgb(43, 11, 61)
+
         webView = WebView(this)
 
-        // السماح بالكوكيز
+        // ============================================================
+        // WEBVIEW / COOKIES
+        // ============================================================
+
         val cookieManager = CookieManager.getInstance()
+
         cookieManager.setAcceptCookie(true)
-        cookieManager.setAcceptThirdPartyCookies(webView, true)
+        cookieManager.setAcceptThirdPartyCookies(
+            webView,
+            true
+        )
+
+        // ============================================================
+        // WEBVIEW SETTINGS
+        // ============================================================
 
         with(webView.settings) {
 
             javaScriptEnabled = true
+
             domStorageEnabled = true
+
             databaseEnabled = true
 
             loadsImagesAutomatically = true
 
             javaScriptCanOpenWindowsAutomatically = true
+
             setSupportMultipleWindows(false)
 
+            cacheMode = WebSettings.LOAD_DEFAULT
+
             allowFileAccess = true
+
             allowContentAccess = true
 
             mixedContentMode =
                 WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
 
-            cacheMode = WebSettings.LOAD_DEFAULT
-
             userAgentString =
                 "$userAgentString TABIBK-Android"
         }
 
-        // تشغيل JavaScript + التنبيهات والنوافذ
+        // ============================================================
+        // WEBVIEW CLIENTS
+        // ============================================================
+
         webView.webViewClient = WebViewClient()
+
         webView.webChromeClient = WebChromeClient()
 
-        webView.loadUrl("https://tabibk2.onrender.com")
+        // ============================================================
+        // منع قص الواجهة تحت شريط الهاتف
+        // ============================================================
+
+        webView.setPadding(
+            0,
+            0,
+            0,
+            0
+        )
+
+        webView.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
+        // ============================================================
+        // تحميل طبيبك
+        // ============================================================
+
+        webView.loadUrl(
+            "https://tabibk2.onrender.com"
+        )
 
         setContentView(webView)
     }
 
+    // ================================================================
+    // زر الرجوع
+    // ================================================================
+
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
+
         if (webView.canGoBack()) {
+
             webView.goBack()
+
         } else {
+
             super.onBackPressed()
         }
     }
